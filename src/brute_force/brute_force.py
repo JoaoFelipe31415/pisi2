4e0a2d02e4c4ac02d661b985a050e00e95fd5623
João Felipe <joaofelipe343@hotmail.com>
2025-10-14 14:00:27 -0300

    nearest_neighbor - Implementation

--- src/brute_force/brute_force.py
+++ src/brute_force/brute_force.py
@@ -1,41 +1,8 @@
 import itertools
 import time
-import os
-
 from tests_cases.build_tests_cases import gerar_casos_berlin52
-def ler_matriz_de_arquivo(nome_arquivo):
-    caminho = os.path.join(os.path.dirname(__file__), '..','..', 'data', nome_arquivo)
-    matriz = []
-    with open(caminho, 'r') as f:
-        primeira_linha = f.readline()
-        M = int(primeira_linha[0])
-        N = int(primeira_linha[1])
-        
-        for _ in range(M):
-            linha_arquivo = f.readline()
-            aux = []
-            for caracter in linha_arquivo:
-                if caracter == "\n":
-                    continue
-                aux.append(caracter)
-            matriz.append(aux)
+from src.utils.utilidade import calcular_distancia_manhattan, encontrar_coordenadas, ler_matriz_de_arquivo
 
-    return M, N, matriz
-
-def encontrar_coordenadas(matriz):
-    """Varre a matriz e retorna um dicionário com as coordenadas de cada ponto."""
-    coordenadas = {}
-    for r, linha in enumerate(matriz):
-        for c, ponto in enumerate(linha):
-            if ponto != '0':
-                coordenadas[ponto] = (r, c) 
-    return coordenadas
-
-def calcular_distancia_manhattan(coord1, coord2):
-    """Calcula a distância entre dois pontos (movimento de drone)."""
-    r1, c1 = coord1
-    r2, c2 = coord2
-    return abs(r1 - r2) + abs(c1 - c2)
 
 def resolver_flyfood_forca_bruta(coordenadas):
     """
